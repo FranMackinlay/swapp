8cc5109ac5b0056e67793378b56d2d25f9cbf2e4
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-25 18:18:24 +0200

    ADD, items details

---
+++ src/components/PlanetDetails/planetDetails.vue
@@ -0,0 +1,15 @@
+<template src="./planetDetails.view.html"></template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'PlanetDetails',
+  props: {
+    item: {},
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./planetDetails.module.less"></style>
---
+++ src/components/StarshipDetails/starshipDetails.vue
@@ -0,0 +1,15 @@
+<template src="./starshipDetails.view.html"></template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'StarshipDetails',
+  props: {
+    item: {},
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./starshipDetails.module.less"></style>
