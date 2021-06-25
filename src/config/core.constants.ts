1573a95988ab58ce6307a209d80f08dd2f63d1db
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-25 15:02:32 +0200

    ADD, core constant

---
+++ src/config/core.constants.ts
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://swapi.dev/api';
+
+const PATHS = {
+  PEOPLE: `${BASE_URL}/people`,
+  STARSHIPS: `${BASE_URL}/starships`,
+  PLANETS: `${BASE_URL}/planets`,
+};
+
+export default PATHS;
