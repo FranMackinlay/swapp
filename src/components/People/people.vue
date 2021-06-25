d760fe3dbbb9f415bc573c94aee269eb58c706df
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-25 17:47:44 +0200

    IMPROVE, people comopnent and service

--- src/components/People/people.vue
+++ src/components/People/people.vue
@@ -2,11 +2,32 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Person } from '@/interfaces/people.interface';
+import Card from '../Card/card.vue';
+import PeopleSrv from '../../services/people.service';
 
 export default Vue.extend({
   name: 'People',
-  props: {
-    msg: String,
+  components: {
+    Card,
+  },
+  data: () => ({
+    people: {},
+  }),
+  methods: {
+    getPeople: async () => {
+      const people = await PeopleSrv.getPeople();
+      return people;
+    },
+  },
+  async created() {
+    const data = await this.getPeople();
+    data.results = data.results.map((item: Person) => {
+      // eslint-disable-next-line no-param-reassign
+      item.url = item.url.replace('https://swapi.dev/api', '');
+      return item;
+    });
+    this.people = data.results;
   },
 });
 </script>
--- src/services/people.service.ts
+++ src/services/people.service.ts
@@ -1,5 +1,5 @@
-import { People } from '@/interfaces/people.interface';
 import axios from 'axios';
+import People from '../interfaces/people.interface';
 import PATHS from '../config/core.constants';
 
 const PeopleSrv = {
