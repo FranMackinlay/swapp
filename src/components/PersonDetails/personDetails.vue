922c72b97ead585d3501b331b5a1024512cb9d24
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-25 19:28:53 +0200

    IMPROVE, person details

--- src/components/PersonDetails/personDetails.vue
+++ src/components/PersonDetails/personDetails.vue
@@ -1,12 +1,31 @@
 <template src="./personDetails.view.html"></template>
 
 <script lang="ts">
+import { Person } from '@/interfaces/people.interface';
 import Vue from 'vue';
+import PeopleSrv from '../../services/people.service';
 
 export default Vue.extend({
   name: 'PersonDetails',
-  props: {
-    item: {},
+  data: () => ({
+    person: {},
+  }),
+  methods: {
+    getPerson: async (id: number): Promise<Person> => {
+      const people = await PeopleSrv.getPerson(id);
+      return people;
+    },
+    normalizeData: (person: Person) => {
+      const normalizedPerson: Person = person;
+      normalizedPerson.homeworld = person.homeworld.replace('https://swapi.dev/api', '');
+      normalizedPerson.starships = normalizedPerson.starships.map((starship: string) => starship.replace('https://swapi.dev/api', ''));
+      return normalizedPerson;
+    },
+  },
+  async created() {
+    let data = await this.getPerson(parseInt(this.$router.currentRoute.params.id, 0));
+    data = this.normalizeData(data);
+    this.person = data;
   },
 });
 </script>
